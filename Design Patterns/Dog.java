64309c423eea8b2170c853c93e0cb035d4c010c8
Mohammed Contractor <mc837@njit.edu>
2020-06-09 00:59:29 -0400

    Lec 2

--- Design Patterns/Dog.java
+++ Design Patterns/Dog.java
@@ -12,4 +12,13 @@ public class Dog extends Animal{
 		randNum = 12;
 		System.out.println("randNum Value : "+randNum);
 	}
+	//cannot call private method in parent class.
+	private void bePrivate(){
+		System.out.println("In a private moethod");
+	}
+	//can use a public method with calls the private method instead.
+	public void accessPrivate(){
+		bePrivate();
+	}
+	
 }
